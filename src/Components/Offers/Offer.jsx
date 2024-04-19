46ab21b87b79b50347f4d749c7ba0c2fbb4c9631
hoducquy0901 <155835444+hoducquy0901@users.noreply.github.com>
2024-04-19 17:54:59 +0700

    component

---
+++ src/Components/Offers/Offer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Offer.css'
+import exclusive from '../../Assets/exclusive.png'
+
+export const Offer = () => {
+  return (
+    <div className='offers'>
+        <div className="offer-left">
+            <h1>Exclusive</h1>
+            <h1>Offers For You</h1>
+            <p>ONLY ON BEST SELLERS PRODUCT</p>
+            <button>Check Now</button>
+        </div>
+        <div className="offer-right">
+            <img src="{}" alt="" />
+        </div>
+    </div>
+  )
+}
