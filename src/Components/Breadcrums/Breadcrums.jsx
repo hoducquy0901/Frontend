66b463a12e927b51a977553baab4388f733102d9
hoducquy0901 <155835444+hoducquy0901@users.noreply.github.com>
2024-04-19 21:57:02 +0700

    product detail

--- src/Components/Breadcrums/Breadcrums.jsx
+++ src/Components/Breadcrums/Breadcrums.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Breadcrums.css'
-export const Breadcrums = () => {
-    const
+import arrow_icon from '../Assets/breadcrum_arrow.png'
+export const Breadcrums = (props) => {
+    const {product} = props;
   return (
-    <div className='breadcrums'>
-        
+    <div className='breadcrum'>
+        HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" />{product.name}
     </div>
   )
 }
---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+        <p>Introducing our stylish black and blue fur jacket, the perfect addition to your winter wardrobe. Crafted with the utmost attention to detail, this jacket features a sleek design that effortlessly combines elegance and warmth. The model showcases the jacket's flattering fit, highlighting its versatility for both casual and formal occasions. Made from high-quality materials, this jacket ensures durability and comfort, making it a must-have for the fashion-forward individual. Stay cozy and chic with our black and blue fur jacket.</p>
+        </div>
+    </div>
+  )
+}
